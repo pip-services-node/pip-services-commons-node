a4741c6840491051cb00a737f6cf6bec8e943391
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-08-02 11:50:56 -0700

    Merge

--- obj/src/run/Opener.js
+++ obj/src/run/Opener.js
@@ -20,11 +20,7 @@ var Opener = /** @class */ (function () {
      *
      * @see [[IOpenable]]
      */
-<<<<<<< HEAD
     Opener.isOpenOne = function (component) {
-=======
-    Opener.isOpen = function (component) {
->>>>>>> 346e56e21539be6cadf7e5a204c52eb8d8473283
         if (_.isFunction(component.isOpen))
             return component.isOpen();
         else
